866f3623098892c39d6f468faa0e456d5fa7b1e6
Amir <42355606+UnicornKilla@users.noreply.github.com>
2025-11-30 16:05:15 +0300

    Created

---
+++ 1.sql
@@ -0,0 +1,11 @@
+-- ALTER TABLE customers
+-- ADD COLUMN amount NUMERIC(12,2);
+
+-- SELECT *
+-- FROM customers;
+
+-- UPDATE customers
+-- SET amount = ROUND( (RANDOM()* 1000)::numeric, 1);
+
+-- SELECT *
+-- FROM customers;
